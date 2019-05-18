266f8942762b34c3321c5a585440505b9020b406
baptiensinh <baptiensinh113@gmail.com>
2019-05-18 23:36:06 +0700

    1

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+var server = require("http").Server(app);
+var url = require('url');
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+app.get("/",async function(req,res){
+  res.render("index");
+});
+
+server.listen(3000);
